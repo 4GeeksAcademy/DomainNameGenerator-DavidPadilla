7bdb51e38d5b459cb94f12b02960143c63e80ed0
David Padilla Villén <149684862+Davpad@users.noreply.github.com>
2024-02-09 10:46:02 +0000

    Domain generator

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let pronoun = ['my','her'];
+let adj = ['gorgeous','inspiring'];
+let noun = ['novel','project'];
+let domain = ['.com','.net','.io','.es']
+
+for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+        for (let h = 0; h < noun.length; h++) {
+            for (let u = 0; u < domain.length; u++) {
+                console.log(pronoun[i]+adj[j]+noun[h]+domain[u]);
+            }
+        }
+    }
+}
